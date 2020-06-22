0e17c8b51be4587a44b29ea56f7b6eb1c450db29
John Meyer <john.meyer@johnmeyer-ltm2.internal.salesforce.com>
2020-06-22 19:53:00 -0400

    Added batch event handling

--- force-app/main/default/lwc/dateShift/dateShift.js
+++ force-app/main/default/lwc/dateShift/dateShift.js
@@ -1,5 +1,6 @@
 import { LightningElement, wire, track } from "lwc";
 import { NavigationMixin } from "lightning/navigation";
+import { subscribe, unsubscribe } from "lightning/empApi";
 import getObjectItems from "@salesforce/apex/DemoDateShifter.getObjectItems";
 import dateShift from "@salesforce/apex/DemoDateShifter.dateShift";
 
@@ -74,6 +75,13 @@ export default class DateShift extends NavigationMixin(LightningElement) {
 						})
 					);
 				});
+				subscribe("/event/Date_Shift_Event__e", -1, (event) => {
+					this.handleBatchEvent(event);
+				}).then((result) => {
+					console.log(`subscribed, result: ${JSON.stringify(result)}`);
+					this.subscription = result;
+				});
+				this.shiftInProgress = true;
 			})
 			.catch((error) => {
 				this.error = error;
@@ -92,5 +100,52 @@ export default class DateShift extends NavigationMixin(LightningElement) {
 		this.objectApiName = event.detail.objectApiName;
 		this.fieldApiName = event.detail.fieldApiName;
 		this.dateOfDemo = event.detail.dateOfDemo;
+	}
+
+	handleBatchEvent(event) {
+		console.log(`got batch event: ${JSON.stringify(event)}`);
+		const sObjectAPI = event.data.payload.SObject_API_Name__c;
+		const runningTotal = event.data.payload.Running_Total__c;
+		const numberOfErrors = event.data.payload.Errors__c;
+		let objectList = this.objectList;
+		let dateShiftFinished = true;
+		let dateShiftHadErrors = false;
+		objectList.forEach((dso) => {
+			if (dso.itemAPIName == sObjectAPI) {
+				dso.itemRunningTotal = runningTotal;
+				dso.itemNumberOfErrors = numberOfErrors;
+				dso.itemShiftFinished = runningTotal >= dso.itemCount;
+				dso.itemRemaining = dso.itemCount - dso.itemRunningTotal;
+				dso.itemPercentage = (100 * dso.itemRunningTotal) / dso.itemCount;
+			}
+			dateShiftFinished = dateShiftFinished && dso.itemShiftFinished;
+			dateShiftHadErrors = dateShiftHadErrors || dso.itemNumberOfErrors > 0;
+		});
+		this.dateShiftFinished = dateShiftFinished;
+		this.dateShiftHadErrors = dateShiftHadErrors;
+		this.objectList = objectList;
+		if (dateShiftFinished) {
+			unsubscribe(this.subscription, (result) => {
+				this.subscription = null;
+			});
+			if (dateShiftHadErrors)
+				this.dispatchEvent(
+					new showToastEvent({
+						mode: "sticky",
+						variant: "error",
+						message: "Errors occurred during the date shift. Please check the system debug log for details.\n" + "All records without errors were date shifted correctly."
+					})
+				);
+			this.dispatchEvent(
+				new showToastEvent({
+					mode: "sticky",
+					variant: "success",
+					title: "Date shifting has completed.",
+					message:
+						"Make sure that you run the Einstein Analytics dataflows that contain the records you shifted so that your dashboards will reflect the shifted dates.\n" +
+						"If you have any questions about how to do that, please consult one of your team's Einstein Analytics Blackbelts."
+				})
+			);
+		}
 	}
 }
