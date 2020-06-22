7d1cfa6a0fa862bcef02bb3f1c179762bc82ae7a
John Meyer <john.meyer@johnmeyer-ltm2.internal.salesforce.com>
2020-06-22 09:39:15 -0400

    More work on date selector

--- force-app/main/default/lwc/dateShiftDateSelector/dateShiftDateSelector.js
+++ force-app/main/default/lwc/dateShiftDateSelector/dateShiftDateSelector.js
@@ -1,6 +1,7 @@
-import { LightningElement, wire, track, api } from "lwc";
+import { LightningElement, wire, track } from "lwc";
 import getOrgObjectList from "@salesforce/apex/DemoDateShifter.getOrgObjectList";
 import getDateTimeFields from "@salesforce/apex/DemoDateShifter.getDateTimeFields";
+import getMinutesToShift from "@salesforce/apex/DemoDateShifter.getMinutesToShift";
 
 export default class DateSelector extends LightningElement {
 	@track orgObjectList = [];
@@ -11,8 +12,15 @@ export default class DateSelector extends LightningElement {
 	fieldSelectorDisabled = true;
 
 	dateOfDemo = new Date(Date.now()).toISOString();
+	dateOfDemoSelected = false;
 
 	loading = true;
+
+	returnedMinutes = 0;
+	minutesToShift = 0;
+	daysToShift = 0;
+	forBack = '';
+	shiftAmountVisible = false;
 
 	error;
 
@@ -41,19 +49,23 @@ export default class DateSelector extends LightningElement {
 	@wire(getDateTimeFields, { objectApiName: "$objectApiName" })
 	wired_getFieldList({ error, data }) {
 		this.fieldList = [];
-		if (data && data.length != 0) {
-			data.forEach((field) => {
-				this.fieldList.push({
-					value: field.apiName,
-					label: field.label
+		if (data) {
+			if (data.length === 0 && this.objectApiName != "") {
+				this.template.querySelector("[data-id='field']").placeholder = "No modifiable DateTime fields";
+			} else {
+				data.forEach((field) => {
+					this.fieldList.push({
+						value: field.apiName,
+						label: field.label
+					});
 				});
-			});
-			this.fieldList.sort((a, b) => (a.label > b.label ? 1 : -1));
-			this.fieldList.unshift({
-				value: "",
-				label: "Select a field"
-			});
-			this.fieldSelectorDisabled = false;
+				this.fieldList.sort((a, b) => (a.label > b.label ? 1 : -1));
+				this.fieldList.unshift({
+					value: "",
+					label: "Select a field"
+				});
+				this.fieldSelectorDisabled = false;
+			}
 		} else if (error) {
 			this.error = error;
 		}
@@ -63,13 +75,27 @@ export default class DateSelector extends LightningElement {
 		this.objectApiName = event.target.value;
 		this.fieldApiName = "";
 		this.fieldSelectorDisabled = this.objectApiName === "";
+		this.shiftAmountVisible = this.fieldApiName != "" && this.dateOfDemoSelected;
 	}
 
 	handleFieldChange(event) {
 		this.fieldApiName = event.target.value;
+		this.shiftAmountVisible = this.fieldApiName != "" && this.dateOfDemoSelected;
 	}
 
 	handleDateChange(event) {
 		this.dateOfDemo = event.target.value;
+		this.dateOfDemoSelected = true;
+		getMinutesToShift({ dateOfDemo: this.dateOfDemo, objectApiName: this.objectApiName, fieldApiName: this.fieldApiName })
+			.then((result) => {
+				this.returnedMinutes = result;
+				this.minutesToShift = Math.abs(this.returnedMinutes);
+				this.datesToShift = Math.abs(this.returnedMinutes) / 60 / 24;
+				this.forBack = this.returnedMinutes < 0 ? "backward" : "forward";
+			})
+			.catch((error) => {
+				this.error = error;
+			});
+		this.shiftAmountVisible = this.fieldApiName != "" && this.dateOfDemoSelected;
 	}
 }
