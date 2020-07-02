b89714e53c962eae903aa2a3c45fb143fdcb6ff8
John Meyer <john.meyer@johnmeyer-ltm2.internal.salesforce.com>
2020-07-02 18:16:07 -0400

    Custom settings work

--- .sfdx/typings/lwc/apex/DemoDateShifter.d.ts
+++ .sfdx/typings/lwc/apex/DemoDateShifter.d.ts
@@ -1,6 +1,12 @@
 declare module "@salesforce/apex/DemoDateShifter.getCustomDateShifterSettings" {
   export default function getCustomDateShifterSettings(): Promise<any>;
 }
+declare module "@salesforce/apex/DemoDateShifter.setCustomDateShifterSettings" {
+  export default function setCustomDateShifterSettings(param: {objectApiName: any, fieldApiName: any}): Promise<any>;
+}
+declare module "@salesforce/apex/DemoDateShifter.deleteCustomDateShifterSettings" {
+  export default function deleteCustomDateShifterSettings(): Promise<any>;
+}
 declare module "@salesforce/apex/DemoDateShifter.getOrgObjectList" {
   export default function getOrgObjectList(): Promise<any>;
 }
